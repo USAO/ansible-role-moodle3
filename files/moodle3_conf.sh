83c1f9387aef3e24169f8a0825d8ad141c8c2ac6
Jason Sherman <jsherman@usao.edu>
2017-09-22 12:49:24 -0500

    Spend just enough time on scripts to get a working moodle migration and upgrade.

--- files/moodle3_conf.sh
+++ files/moodle3_conf.sh
@@ -1,8 +1,9 @@
 ## Read useful values out of the config file
 moodle3PARENT=`dirname ${1}`
-moodle3DIR=`readlink -f ${1}/moodle3`
+moodle3DIR=`readlink -f ${1}/moodle`
 moodle3CFG=${moodle3DIR}/config.php
 moodle3DBHOST=`cat $moodle3CFG | grep '^$CFG->dbhost' | cut -d "=" -f 2 | cut -d ';' -f 1 | xargs`
 moodle3DB=`cat $moodle3CFG | grep '^$CFG->dbname' | cut -d "=" -f 2 | cut -d ';' -f 1 | xargs`
 moodle3USER=`cat $moodle3CFG | grep '^$CFG->dbuser' | cut -d "=" -f 2 | cut -d ';' -f 1 | xargs`
 moodle3PASS=`cat $moodle3CFG | grep '^$CFG->dbpass' | cut -d "=" -f 2 | cut -d ';' -f 1 | xargs`
+moodle3wwwroot=`cat $moodle3CFG | grep '^$CFG->wwwroot' | cut -d "=" -f 2 | cut -d ';' -f 1 | xargs`
---
+++ files/moodle3_migrate.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+
+PATH=/opt/usao/moodle3/bin:/usr/local/bin:/usr/bin:/bin:/sbin:$PATH
+
+source /opt/usao/moodle3/etc/moodle3_conf.sh
+
+## Require arguments
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] ; then
+    cat <<USAGE
+moodle3_migrate.sh migrates a site between hosts.
+Usage: moodle3_migrate.sh \$dest_moodledir \$src_moodlehost \$src_cfgdir
+\$dest_moodledir          local dir for Moodle site (eg. /srv/example).
+\$src_moodlehost           host of site to migrate
+\$src_cfgdir   remote dir of site to migrate on \$src_moodlehost
+USAGE
+    exit 1;
+fi
+
+dest_moodledir=${1}
+src_moodlehost=${2}
+src_cfgdir=${3}
+dest_basename=$(basename "$dest_moodledir")
+
+# Read src config file
+src_cfg=$(ssh ${src_moodlehost} cat ${src_cfgdir}/config.php)
+
+# Set vars from it.
+src_cfg=/tmp/config.php
+src_dbhost=`cat $src_cfg | grep '^$CFG->dbhost' | cut -d "=" -f 2 | cut -d ';' -f 1 | xargs`
+src_dbname=`cat $src_cfg | grep '^$CFG->dbname' | cut -d "=" -f 2 | cut -d ';' -f 1 | xargs`
+src_dbuser=`cat $src_cfg | grep '^$CFG->dbuser' | cut -d "=" -f 2 | cut -d ';' -f 1 | xargs`
+src_dbpass=`cat $src_cfg | grep '^$CFG->dbpass' | cut -d "=" -f 2 | cut -d ';' -f 1 | xargs`
+src_wwwroot=`cat $src_cfg | grep '^$CFG->wwwroot' | cut -d "=" -f 2 | cut -d ';' -f 1 | xargs`
+src_dataroot=`cat $src_cfg | grep '^$CFG->dataroot' | cut -d "=" -f 2 | cut -d ';' -f 1 | xargs`
+
+
+# Sync over the moodledata
+rsync -avz ${src_moodlehost}:${src_dataroot}/ ${dest_moodledir}/moodledata/
+
+# Dump src db to local file in dest_moodledir
+ssh ${src_moodlehost} mysqldump --single-transaction --lock-tables=false --allow-keywords --opt -h${src_dbhost} -u${src_dbuser} -p${src_dbpass} ${src_dbname} >${dest_moodledir}/db/moodle3_${src_moodlehost}_dump.sql
+
+# change sitename in db dump.
+sed -e "s#${src_wwwroot}#${moodle3wwwroot}#g" ${dest_moodledir}/db/moodle3_${src_moodlehost}_dump.sql > ${dest_moodledir}/db/moodle3_${dest_basename}_dump.sql
