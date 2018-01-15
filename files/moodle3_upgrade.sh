a61a1d1b442b59ac6e67024106fa420cc2783b27
Jason Sherman <jsherman@usao.edu>
2018-01-15 12:46:43 -0600

    Naive s/php56/php71/g in yml and j2 files.

--- files/moodle3_upgrade.sh
+++ files/moodle3_upgrade.sh
@@ -5,7 +5,7 @@ PATH=/opt/usao/moodle3/bin:/usr/local/bin:/usr/bin:/bin:/sbin:$PATH
 source /opt/usao/moodle3/etc/moodle3_conf.sh
 
 sudo -u apache bash -c "\
-source scl_source enable rh-php56; \
+source scl_source enable rh-php71; \
 php ${moodle3DIR}/admin/cli/mysql_compressed_rows.php --fix; \
 php ${moodle3DIR}/admin/cli/maintenance.php --enable; \
 php ${moodle3DIR}/admin/cli/upgrade.php --non-interactive; \
